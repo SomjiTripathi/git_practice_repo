e6c2760e7b9931f3f38b0e3d81e64b43f051bd92
Somji Tripathi <sjzbagno3@gmail.com>
2022-12-09 19:09:07 +0530

    adding security files

---
+++ src/main/java/com/example/demo/AppSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter
+{
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		
+		List<UserDetails> users =new ArrayList<>();
+		users.add(User.withDefaultPasswordEncoder().username("somji").password("1234").roles("USER").build());
+		users.add(User.withDefaultPasswordEncoder().username("guddu").password("12345").roles("user").build());
+		return new InMemoryUserDetailsManager(users);
+	}
+	
+	
+
+}
